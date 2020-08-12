50c2b825d2e0b15676af945bfb426798a0d847a3
Esneider <esneider_98@outlook.com>
2020-08-12 18:42:33 -0500

    seeder con socios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,25 +20,43 @@ class UsersTableSeeder extends Seeder
         $admin->password = bcrypt('admin123');
         $admin->save();
 
-        $socio = new User();
-        $socio->nombres = "Francisco Joel";
-        $socio->apellidos = "Marin Calderón";
-        $socio->cedula = "1315311009";
-        $socio->email = "socio@socio.com";
-        $socio->password = bcrypt('socio123');
-        $socio->save();
+        $admin1 = new User();
+        $admin1->nombres = "Francisco Joel";
+        $admin1->apellidos = "Marin Calderón";
+        $admin1->cedula = "1315311009";
+        $admin1->email = "socio@socio.com";
+        $admin1->password = bcrypt('socio123');
+        $admin1->save();
 
-        $moderador = new User();
-        $moderador->nombres = "Martita Julieta";
-        $moderador->apellidos = "Kardashian Mendoza";
-        $moderador->cedula = "0987654321";
-        $moderador->email = "moderador@moderador.com";
-        $moderador->password = bcrypt('moderador');
-        $moderador->save();
+        $socio1 = new User();
+        $socio1->nombres = "Martita Julieta";
+        $socio1->apellidos = "Kardashian Mendoza";
+        $socio1->cedula = "0987654321";
+        $socio1->email = "moderador@moderador.com";
+        $socio1->password = bcrypt('12345');
+        $socio1->save();
+
+        $socio2 = new User();
+        $socio2->nombres = "Jose Jair";
+        $socio2->apellidos = "Santander Mendoza";
+        $socio2->cedula = "1111111111";
+        $socio2->email = "moderador@moderador.com";
+        $socio2->password = bcrypt('12345');
+        $socio2->save();
+
+        $socio3 = new User();
+        $socio3->nombres = "Maria Karen";
+        $socio3->apellidos = "Estrella de la Mañana";
+        $socio3->cedula = "2222222222";
+        $socio3->email = "moderador@moderador.com";
+        $socio3->password = bcrypt('12345');
+        $socio3->save();
 
         $admin->assignRole('Administrador');
-        $socio->assignRole('Administrador');
-        $moderador->assignRole('Moderador');
+        $admin1->assignRole('Administrador');
+        $socio1->assignRole('Socio');
+        $socio2->assignRole('Socio');
+        $socio3->assignRole('Socio');
 
     }
 }
