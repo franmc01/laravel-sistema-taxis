0fa4820ec4c549f194cdfd973b086f41ca20bc7d
francisco-1998 <francmarinc@gmail.com>
2020-09-10 08:18:17 -0500

    Terminada nostros

--- database/seeds/PaginaPrincipalTableSeeder.php
+++ database/seeds/PaginaPrincipalTableSeeder.php
@@ -13,10 +13,10 @@ class PaginaPrincipalTableSeeder extends Seeder
     public function run()
     {
         $pp = new PaginaPrincipal();
-        $pp->historia = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, ad unde eaque. Rem numquam cumque, voluptate, dolor iure, repudiandae officiis voluptates aspernatur sapiente quisquam fugit!';
-        $pp->mision = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, ad unde eaque. Rem numquam cumque, voluptate, dolor iure, repudiandae officiis voluptates aspernatur sapiente quisquam fugit!';
-        $pp->vision = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, ad unde eaque. Rem numquam cumque, voluptate, dolor iure, repudiandae officiis voluptates aspernatur sapiente quisquam fugit!';
-        $pp->direccion = 'Calle Salinas Y Granda Centeno Diagonal al TIA S.A';
+        $pp->historia = 'La Compañía de Taxi Ejecutivo TRANSLASINPAR S.A empezó a funcionar en el año 2013 y ha brindado  sus servicios a la población de la ciudad de Calceta a través de los años siempre con esmero y siendo una compañía responsable y buscando brindar el mejor servicio posible. En el año 2018 cambia su nombre a Compañía de Taxi Convencional TRANS LASINPAR S.A. En la actualidad cuenta con 35 vehículos a disposición de la ciudadanía y cuenta con 4 bases de trabajo a lo largo de la ciudad de Calceta.';
+        $pp->mision = 'Dar una respuesta segura y rápida para satisfacer la demanda de transporte de nuestros clientes, de la mano de los mejores precios y el mejor personal de trabajo.';
+        $pp->vision = 'Nuestro principal objetivo es brindar un proceso con total cumplimiento y seguridad, garantizando siempre el respeto y atención que se merecen los clientes.';
+        $pp->direccion = 'Calle Salinas Y Granda Centeno frente al TIA S.A';
         $pp->telefono1 = '052 686 169';
         $pp->telefono2 = '052 686 786';
         $pp->correo_contacto = 'tejecutivo_translasinpar@hotmail.com';
--- resources/views/Paginaprincipal/partials/nosotros.blade.php
+++ resources/views/Paginaprincipal/partials/nosotros.blade.php
@@ -10,7 +10,7 @@
                     <h2>HISTORIA</h2>
                 </div>
                 <div class="about-text-right">
-                    <p>Aqui va la historia {{ $info_pagina[0]->historia }}</p>
+                    <p>{{ $info_pagina[0]->historia }}</p>
                     <div id="skills">
                         <div class="row">
                             <div class="col-md-6">
@@ -18,7 +18,7 @@
                                     <h2>MISIÓN</h2>
                                 </div>
                                 <div class="skillbar" data-percent="95%" style="text-align: justify">
-                                    <p>Aqui va la mision {{ $info_pagina[0]->mision }}</p>
+                                    <p>{{ $info_pagina[0]->mision }}</p>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -26,7 +26,7 @@
                                     <h2>VISIÓN</h2>
                                 </div>
                                 <div class="skillbar" data-percent="95%" style="text-align: justify">
-                                    <p>Aqui va la vision {{ $info_pagina[0]->vision }}</p>
+                                    <p>{{ $info_pagina[0]->vision }}</p>
                                 </div>
                             </div>
                         </div>
